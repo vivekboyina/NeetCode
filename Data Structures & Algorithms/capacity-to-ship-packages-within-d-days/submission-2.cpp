a315eac8da58b8ea716a6f3a11ef7245e924b367
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-07-26 21:19:34 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-2

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& wei, int days) {
+        int low = INT_MIN;
+        int high = 0;
+        for(int i : wei)
+        {
+            low = max(low,i);
+            high += i;
+        }
+        int mid;
+        int ans = INT_MAX;
+        int cnt = 0;
+        int w = 0;
+        while(low <= high)
+        {
+            mid = low + (high - low)/2;
+            cnt = 0;
+            w = 0;
+            for(int i : wei)
+            {
+                w+=i;
+                if(w > mid)
+                {
+                    cnt++;
+                    w = i;
+                }
+            }
+            if(w > 0) cnt++;
+            ans = min(w,ans);
+            if(cnt > days) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+};
