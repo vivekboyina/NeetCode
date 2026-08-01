05fa567f32bf4eacc992bbaaeb2a0847580bdc70
Boyina Veera Venkata Vivek <vivekboyina77@gmail.com>
2026-08-01 19:50:58 +0530

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        stack<ListNode*>st;
+        ListNode* cc = head;
+        while(cc != nullptr)
+        {
+            st.push(cc);
+            cc = cc -> next;
+        }
+        int n = (st.size() + 1)/2;
+        cc = head;
+        ListNode* tmp;
+        while(n > 0)
+        {
+            tmp = cc -> next;
+            cc -> next = st.top();
+            cc = cc -> next;
+            cc -> next = tmp;
+            st.pop();
+            n-=1;
+            if(n > 0) cc = cc -> next;
+            else cc -> next = nullptr;
+        }
+    }
+};
